66694e707da308c6560640f37e2d0e93f1e34d8b
AmirHusseini <56257612+AmirHusseini@users.noreply.github.com>
2022-04-18 00:46:34 +0200

    Dashbord updated!

--- Web Bank/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Web Bank/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -137,7 +137,7 @@ namespace Web_Bank.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, "Customer");
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
